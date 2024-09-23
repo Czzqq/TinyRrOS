230b2b76cbce382dc4bfbc4a2f1b7c9745377c46
Czzqq <chenlthree@gmail.com>
2024-09-23 23:28:38 +0800

    added uart16550

--- os/src/main.rs
+++ os/src/main.rs
@@ -4,6 +4,13 @@
 use core::arch::global_asm;
 global_asm!(include_str!("asm/entry.asm"));
 
+mod io;
+mod drivers {
+    pub mod serial {
+        pub mod uart16550;
+    }
+}
+
 fn clear_bss() {
     extern "C" {
         fn sbss();
@@ -24,9 +31,15 @@ fn clear_bss() {
     }
 }
 
+use drivers::serial::uart16550::uart_init;
+use drivers::serial::uart16550::uart_send_string;
 #[no_mangle]
 pub extern "C" fn kernel_main() -> ! {
     clear_bss();
+
+    uart_init();
+    uart_send_string("Hello, TinyRrOS!\n");
+
     loop{}
 }
 
