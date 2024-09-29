be577ac46535105274a3e1b7af29ddc188d545f1
Czzqq <chenlthree@gmail.com>
2024-09-30 00:03:25 +0800

    os: added sbi call && sbi: added exception vector

---
+++ bootloader/sbi_trap.c
@@ -0,0 +1,48 @@
+#include "include/csr.h"
+#include "include/sbi_trap_regs.h"
+#include "sbi_trap.h"
+
+extern void sbi_exception_vector(void);
+
+void sbi_trap_init(void)
+{
+    /* set exception vection base address */
+    write_csr(mtvec, sbi_exception_vector);
+
+    /* close all int */
+    write_csr(mie, 0);
+}
+
+static int sbi_ecall_handle(unsigned int id, struct sbi_trap_regs *regs)
+{
+    int ret = 0;
+
+    switch (id) {
+        case SBI_CONSOLE_PUTCHAR:
+            ret = 1;
+            break;
+        default:
+            break;
+    }
+
+    if (!ret)
+        regs->mepc += 4;
+
+    return ret;
+}
+
+void sbi_trap_handler(struct sbi_trap_regs *regs)
+{
+    int ret = 0;
+
+    unsigned long mcause = read_csr(mcause);
+    unsigned long ecall_id = regs->a7;
+
+    switch (mcause) {
+        case CAUSE_SUPERVISOR_ECALL:
+            sbi_ecall_handle(ecall_id, regs);
+            break;
+        default:
+            break;
+    }
+}
---
+++ os/src/sbi.rs
@@ -0,0 +1,32 @@
+#![allow(dead_code)]
+use core::arch::asm;
+
+const SBI_CONSOLE_PUTCHAR: usize = 0x1;
+const SBI_CONSOLE_GETCHAR: usize = 0x2;
+
+fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize,) -> usize {
+    let ret;
+    unsafe {
+        asm!(
+            "ecall",
+            in("a0") arg0,
+            in("a1") arg1,
+            in("a2") arg2,
+            in("a7") which,
+            lateout("a0") ret,
+            options(nostack),
+            );
+    }
+    ret
+}
+
+#[inline(always)]
+pub fn sbi_putchar(c: usize) {
+    sbi_call(SBI_CONSOLE_PUTCHAR, c, 0, 0);
+}
+
+pub fn sbi_putstring(s: &str) {
+    for c in s.chars() {
+        sbi_call(SBI_CONSOLE_GETCHAR, c as usize, 0, 0);
+    }
+}
