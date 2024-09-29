8faf5ff399efdcb39d4cabdc4736f24877f6eda0
Czzqq <chenlthree@gmail.com>
2024-09-29 08:49:02 +0800

    added memory.rs

---
+++ os/src/memory.rs
@@ -0,0 +1,45 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use core::arch::asm;
+
+unsafe fn memcpy_asm(dst: *mut u8, src: *const u8, size: usize) {
+    let _tmp: usize;
+    let end = src.add(size);
+
+    unsafe {
+        asm!(
+            "1: ld {1}, 0({2})",
+            "sd {1}, 0({0})",
+            "addi {0}, {0}, 8",
+            "addi {2}, {2}, 8",
+            "blt {2}, {3}, 1b",
+            /*
+             * it's mean the dst variables inout,
+             * don't care about the final value of `dst`
+             */
+            inout(reg) dst => _,
+            lateout(reg) _tmp,
+            inout(reg) src => _,
+            in(reg) end,
+        );
+    }
+    return
+    //for i in 0..size {
+    //    asm!(
+    //        "lb t0, 0({src_ptr})", // Load byte from source 
+    //        "sb t0, 0({dst_ptr})", // Store byte to destination 
+    //        src_ptr = in(reg) src.add(i),
+    //        dst_ptr = in(reg) dst.add(i),
+    //        out("t0") _,
+    //    );
+    //}
+}
+
+pub fn memcpy(dst: *mut u8, src: *const u8, size: usize) {
+    unsafe {
+        memcpy_asm(dst, src, size);
+    }
+
+    return
+}
