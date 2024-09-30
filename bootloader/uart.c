3a1756fef9fb04607332c2dc82d2282639460262
chenzheng <chenzheng@trustkernel.com>
2024-09-30 13:49:10 +0800

    fixup comments

--- bootloader/uart.c
+++ bootloader/uart.c
@@ -4,26 +4,25 @@
 #define UART        0x10000000
 
 /* THR:transmitter holding register */
-#define UART_DAT    (UART+0x00) /* 数据寄存器*/
-#define UART_IER    (UART+0x01) /* 中断使能寄存器*/
-#define UART_IIR    (UART+0x02) /* 中断标识寄存器 (read only)*/
-#define UART_FCR    (UART+0x02) /* FIFO控制寄存器 (write only)*/
-#define UART_LCR    (UART+0x03) /* 线路控制寄存器*/
-#define UART_MCR    (UART+0x04) /* MODEN控制寄存器*/
-#define UART_LSR    (UART+0x05) /* 线路状态寄存器*/
-#define UART_MSR    (UART+0x06) /* MODEN状态寄存器*/
+#define UART_DAT    (UART+0x00) /* data register */
+#define UART_IER    (UART+0x01) /* interrupt enable register */
+#define UART_IIR    (UART+0x02) /* interrupt falg register (read only)*/
+#define UART_FCR    (UART+0x02) /* FIFO control register (write only)*/
+#define UART_LCR    (UART+0x03) /* line control register */
+#define UART_MCR    (UART+0x04) /* MODEN control register */
+#define UART_LSR    (UART+0x05) /* line status register */
+#define UART_MSR    (UART+0x06) /* MODEN status register */
+#define UART_DLL (UART+0x00) /* Prescaler register low 8 bits */
+#define UART_DLM (UART+0x01) /* Prescaler register high 8 bits */
 
-#define UART_DLL (UART+0x00)  /*预分频寄存器低8位*/
-#define UART_DLM (UART+0x01)  /*预分频寄存器高8位*/
-
-#define UART_LSR_ERROR   0x80  /* 出错 */
-#define UART_LSR_EMPTY    0x40  /* 传输FIFO和移位寄存器为空 */
-#define UART_LSR_TFE	    0x20  /* 传输FIFO为空 */
-#define UART_LSR_BI	    0x10  /* 传输被打断 */
-#define UART_LSR_FE	    0x08  /* 接收到没有停止位的帧 */
-#define UART_LSR_PE	    0x04  /* 奇偶校验错误位 */
-#define UART_LSR_OE	    0x02  /* 数据溢出 */
-#define UART_LSR_DR	    0x01  /* FIFO有数据 */
+#define UART_LSR_ERROR 0x80 /* error */
+#define UART_LSR_EMPTY 0x40 /* FIFO and shift register are empty */
+#define UART_LSR_TFE 0x20 /* Transmit FIFO is empty */
+#define UART_LSR_BI 0x10 /* Transmission interrupted */
+#define UART_LSR_FE 0x08 /* Frame received without stop bit */
+#define UART_LSR_PE 0x04 /* Parity error bit */
+#define UART_LSR_OE 0x02 /* Data overflow */
+#define UART_LSR_DR 0x01 /* FIFO has data */
 
 void sbi_uart_send(char c)
 {
@@ -66,7 +65,7 @@ void sbi_uart_init()
 	/*8 bits, no parity, one stop bit*/
 	writeb(0x3, UART_LCR);
 
-	/* 使能FIFO，清空FIFO，设置14字节threshold*/
+	/* enable FIFO，clear FIFO，set 14-byte threshold */
 	writeb(0xc7, UART_FCR);
 
     return;
