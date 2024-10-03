d83c4314787d5cee8b7cb09cd453420043d45c0e
Czzqq <chenlthree@gmail.com>
2024-10-03 23:19:35 +0800

    added timer.rs

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -1,10 +1,11 @@
 #![allow(dead_code)]
 use core::arch::asm;
 
+const SBI_SET_TIMER: usize = 0x0;
 const SBI_CONSOLE_PUTCHAR: usize = 0x1;
 const SBI_CONSOLE_GETCHAR: usize = 0x2;
 
-fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize,) -> usize {
+fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     let ret;
     unsafe {
         asm!(
@@ -29,4 +30,8 @@ pub fn sbi_putstring(s: &str) {
     for c in s.chars() {
         sbi_call(SBI_CONSOLE_PUTCHAR, c as usize, 0, 0);
     }
+}
+
+pub fn sbi_set_timer(stime_val: usize) {
+    sbi_call(SBI_SET_TIMER, stime_val, 0, 0);
 }
