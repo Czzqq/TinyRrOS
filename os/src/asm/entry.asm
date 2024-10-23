80e116d02131d110f078470601629aa795f7a927
Czzqq <chenlthree@gmail.com>
2024-10-23 20:22:39 +0800

    fix comflict

--- os/src/asm/entry.asm
+++ os/src/asm/entry.asm
@@ -37,64 +37,6 @@
 	/*save sepc*/
 	csrr s2, sepc
 	sd s2, 0(sp)
-<<<<<<< HEAD
-	
-	/*save sbadaddr*/
-	csrr s3, stval
-	sd s3, 264(sp)
-
-	/*save scause*/
-	csrr s4, scause
-	sd s4, 272(sp)
-
-	/*save ssratch*/
-	csrr s5, sscratch
-	sd s5, 32(sp)
-
-	/*save SP*/
-	addi s0, sp, 288
-	sd s0, 16(sp)
-.endm
-
-.macro kernel_exit
-	ld a0, 256(sp)
-	csrw sstatus, a0
-
-	ld a2, 0(sp)
-	csrw sepc, a2
-
-	ld x1,  8(sp)
-	ld x3,  24(sp)
-	ld x4,  32(sp)
-	ld x5,  40(sp)
-	ld x6,  48(sp)
-	ld x7,  56(sp)
-	ld x8,  64(sp)
-	ld x9,  72(sp)
-	ld x10, 80(sp)
-	ld x11, 88(sp)
-	ld x12, 96(sp)
-	ld x13, 104(sp)
-	ld x14, 112(sp)
-	ld x15, 120(sp)
-	ld x16, 128(sp)
-	ld x17, 136(sp)
-	ld x18, 144(sp)
-	ld x19, 152(sp)
-	ld x20, 160(sp)
-	ld x21, 168(sp)
-	ld x22, 176(sp)
-	ld x23, 184(sp)
-	ld x24, 192(sp)
-	ld x25, 200(sp)
-	ld x26, 208(sp)
-	ld x27, 216(sp)
-	ld x28, 224(sp)
-	ld x29, 232(sp)
-	ld x30, 240(sp)
-	ld x31, 248(sp)
-
-=======
 
 	/*save sbadaddr*/
 	csrr s3, stval
@@ -151,18 +93,12 @@
 	ld x30, 240(sp)
 	ld x31, 248(sp)
 
->>>>>>> dev/backtrace
 	ld x2,  16(sp)
 .endm
 
 .align 2
 .global trigger_fault
 trigger_fault:
-<<<<<<< HEAD
-    li a0, 0x70000
-    ld a0, (a0)
-    ret
-=======
     # set a vaild sp
     addi sp, sp, -16
     sd ra, 8(sp)
@@ -182,7 +118,6 @@ trigger_fault:
     addi sp, sp, 16
     ret
 
->>>>>>> dev/backtrace
 /*
  * do_exception_vector must align 4 bytes
  */
